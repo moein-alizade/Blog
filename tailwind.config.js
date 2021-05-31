91769bce366d94d1cae17c39987761414331a560
moein58010 <m.alizade58010@gmail.com>
2021-06-01 03:12:39 +0430

    Init laravel project with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // purge: {
+    //     enabled: false,
+    //     content: [
+    //         './resources/views/**/*.blade.php',
+    //     ]
+    // },
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            width: {
+                '200': '50rem'
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
